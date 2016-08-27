e685f93fce4f7425f8508f7384d6ca55787d04cc
Calvin <Calvin.lacc@gmail.com>
2016-08-27 17:50:20 -0600

    Resources for get and post request have been created and are working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show 
+    @user = User.find(params[:id])
+  end
+
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(my_params)
+    redirect_to users_path
+  end
+
+  private
+
+  def my_params
+    params.require(:user).permit(:username, :email)
+  end
+
 end
